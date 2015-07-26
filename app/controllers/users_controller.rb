b8dea3232ff31642eced84c8f476a2945a5c23cd
behindwisterias <send2goto@gmail.com>
2015-07-26 13:20:39 +0000

    user-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,29 +1,62 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController 
+  before_action :set_user, only: [:edit, :update]
   
+  def edit #課題対応で追加 
+    @user = User.find(params[:id])
+  end
   
-  def show # 追加
-   @user = User.find(params[:id])
+  def update #課題対応で追加
+    if @user.update(user_profile)
+      flash[:success] = "Updated profile"
+      redirect_to @user
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+  
+  def show
+    @user = User.find(params[:id])
   end
   
   def new
     @user = User.new
   end
-
+  
+  #def create
+  #  @user = User.find_by(email: params[:session][:email].downcase)
+  #  if @user && @user.authenticate(params[:session][:password])
+  #    session[:user_id] = @user.id
+  #    flash[:info] = "logged in as #{@user.name}"
+  #    redirect_to @user
+  #  else
+  #    flash[:danger] = 'invalid email/password combination'
+  #    render 'new'
+  #  end
+  #end
+  
   def create
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # ここを修正
+      redirect_to @user
     else
       render 'new'
     end
   end
 
   private
-
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                  :password_confirmation)
+  end
+  
+  def user_profile
+    params.require(:user).permit(:name, :email, :profile)
+  end
+  
+  def set_user
+    @user = User.find(params[:id])
   end
   
   
---
+++ db/migrate/20150726130433_add_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :profile, :string
+  end
+end
